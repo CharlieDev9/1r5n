a5d31a015c1835ea76c19761cddf97c4442f78a2
Charlie Dev <83170854+CharlieDev9@users.noreply.github.com>
2022-11-02 10:32:44 +0000

    Update Three.sh

--- Three.sh
+++ Three.sh
@@ -2,16 +2,16 @@ sudo pacman -Ss xf86-video-amdgpu
 sudo pacman -S mesa xorg xorg-server plasma alsa-utils vulkan-swrast vulkan-tools alsa-firmware alsa-plugins firefox konsole dolphin vlc mpv inkscape telegram-desktop gwenview kdenlive gparted htop gvfs-mtp wget ark unrar unzip p7zip
 
 sudo pacman -S mesa xorg xorg-server xorg-xinit alsa-utils vulkan-swrast vulkan-tools alsa-firmware alsa-plugins firefox konsole dolphin vlc telegram-desktop gwenview gparted htop gvfs-mtp wget ark unrar unzip p7zip
-
-sudo pacman -S i3-gaps neofetch fish dmenu flameshot urxvt htop sddm nitrogen picom
+sudo pacman -S --needed lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader
+sudo pacman -S neofetch dmenu flameshot urxvt htop sddm
 
 
 sudo systemctl enable sddm
 
-git clone https://aur.archlinux.org/visual-studio-code-bin.git
-cd visual-studio-code-bin
-makepkg -si
-cd
+#git clone https://aur.archlinux.org/visual-studio-code-bin.git
+#cd visual-studio-code-bin
+#makepkg -si
+#cd
 
 
 #git clone https://aur.archlinux.org/figma-linux.git
@@ -20,28 +20,28 @@ cd
 #cd
 
 
-git clone https://aur.archlinux.org/lightly-qt.git
-cd lightly-qt
-makepkg -si
-cd
+#git clone https://aur.archlinux.org/lightly-qt.git
+#cd lightly-qt
+#makepkg -si
+#cd
 
 
-git clone https://aur.archlinux.org/aic94xx-firmware.git
-cd aic94xx-firmware
-makepkg -si
-cd
+#git clone https://aur.archlinux.org/aic94xx-firmware.git
+#cd aic94xx-firmware
+#makepkg -si
+#cd
 
 
-git clone https://aur.archlinux.org/wd719x-firmware.git
-cd wd719x-firmware
-makepkg -si
-cd
+#git clone https://aur.archlinux.org/wd719x-firmware.git
+#cd wd719x-firmware
+#makepkg -si
+#cd
 
 
-git clone https://aur.archlinux.org/latte-dock-git.git
-cd latte-dock-git
-makepkg -si
-cd
+#git clone https://aur.archlinux.org/latte-dock-git.git
+#cd latte-dock-git
+#makepkg -si
+#d
 
 
 #git clone https://aur.archlinux.org/protonvpn-cli-ng.git
@@ -55,9 +55,9 @@ cd
 #makepkg -si
 #cd
 
-cd /home/charlie/Charlie/Apps
-sudo chmod +x ./Xdm.sh
-sudo ./Xdm.sh
+#cd /home/charlie/Charlie/Apps
+#sudo chmod +x ./Xdm.sh
+#sudo ./Xdm.sh
 
-wget https://mail.tutanota.com/desktop/tutanota-desktop-linux.AppImage
+#wget https://mail.tutanota.com/desktop/tutanota-desktop-linux.AppImage
 #sudo chmod +x ./tutanota-desktop-linux.AppImage
