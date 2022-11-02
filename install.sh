c58e35c27dbb68dbc503865d1d3515b18685063c
Charlie Dev <83170854+CharlieDev9@users.noreply.github.com>
2022-11-02 10:27:35 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-cryptsetup open /dev/sda3 mecw
+#cryptsetup open /dev/sda3 mecw
 
-mkfs.ext4 /dev/vg/root
-mkswap /dev/vg/swap
+#mkfs.ext4 /dev/vg/root
+#mkswap /dev/vg/swap
 
-sleep 2
+#sleep 2
 
-mount /dev/vg/root /mnt
-mkdir /mnt/home
-mount /dev/vg/home /mnt/home
-swapon /dev/vg/swap
+#mount /dev/vg/root /mnt
+#mkdir /mnt/home
+#mount /dev/vg/home /mnt/home
+#swapon /dev/vg/swap
 
-sleep 2
+#sleep 2
 
-mkfs.fat -F32 /dev/sda2
+#mkfs.fat -F32 /dev/sda2
 
 
-pacstrap /mnt base base-devel linux-lts linux-firmware mkinitcpio lvm2 vim nano networkmanager dhcpcd wpa_supplicant linux-lts-headers reflector
+pacstrap /mnt base base-devel linux-lts linux-firmware mkinitcpio vim nano networkmanager dhcpcd wpa_supplicant linux-lts-headers reflector
 
-sleep 3
+#sleep 3
 
-genfstab -U /mnt >> /mnt/etc/fstab
+#genfstab -U /mnt >> /mnt/etc/fstab
 
-clear
+#clear
 
-echo "noatime"
-sleep 3
+#echo "noatime"
+#sleep 3
 
-nano /mnt/etc/fstab
+#nano /mnt/etc/fstab
 
-arch-chroot /mnt
+#arch-chroot /mnt
 
